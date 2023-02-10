70e2004fe47ff04982d30c752d5a208514288d12
Chris Walter <walt50@gmail.com>
2023-02-09 21:12:53 -0500

    Initial Commit

---
+++ APIAppSwiftUI/APIAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIAppSwiftUIApp.swift
+//  APIAppSwiftUI
+//
+//  Created by Christopher Walter on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct APIAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
